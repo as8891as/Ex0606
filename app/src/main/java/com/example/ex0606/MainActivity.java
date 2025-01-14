4a7aa80d3e7aedc92b0198ac425349acfdadd074
as8891as <as8891@bs.amalnet.k12.il>
2025-01-14 20:59:57 +0200

    Button functionality complete

--- app/src/main/java/com/example/ex0606/MainActivity.java
+++ app/src/main/java/com/example/ex0606/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex0606;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageV;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageV=findViewById(R.id.imageV);
+        btn=findViewById(R.id.btn);
+    }
+
+    public void pressed(View view) {
+        imageV.setImageResource(R.drawable.image1);
     }
 }
